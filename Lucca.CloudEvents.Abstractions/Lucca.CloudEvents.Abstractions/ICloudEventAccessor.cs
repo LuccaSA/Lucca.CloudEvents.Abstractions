c985572a975482ebec197a094fbfb69a940134f5
Stéphane Seguin <sseguin@lucca.fr>
2020-02-10 16:06:33 +0100

    Add CloudEventAccessor

---
+++ Lucca.CloudEvents.Abstractions/Lucca.CloudEvents.Abstractions/ICloudEventAccessor.cs
@@ -0,0 +1,9 @@
+﻿using CloudNative.CloudEvents;
+
+namespace Lucca.CloudEvents.Abstractions
+{
+    public interface ICloudEventAccessor
+    {
+        CloudEvent CloudEvent { get; set; }
+    }
+}
